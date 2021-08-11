ecac42389b4a873cd6aad1d2e37a1fed172fb2d0
Ishya Casukhela <casukhelai@gmail.com>
2021-08-11 17:26:59 -0400

    fixed spelling errors and added clearer variable declarations

--- controllers/api/apiroutes.js
+++ controllers/api/apiroutes.js
@@ -5,53 +5,53 @@ const { v4: uuidv4 } = require('uuid');
 
 //Reading the jsonfile
 //Replace html front end filename here//db.json is going to be  under the existing db folder
-router.get('/api/Filename', (req,res) => {
-       let grocerydata= JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+router.get('/api/homepage', (req,res) => {
+       let groceryData= JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
-       res.json(grocerydata);
+       res.json(groceryData);
 });
 
 // POST request OR Create //Replace filename here
-router.post('/api/Filename-html front end', (req,res) => {
+router.post('/api/homepage', (req,res) => {
     //get  grocery item from end user  (client end)
-    const newGroceryitem = req.body;
+    const newGroceryItem = req.body;
 
     console.log("newly created grocery item");
 
     // npm package for uniqueid uuid
-    newgroceryitem.id = uuidv4();
+    newGroceryItem.id = uuidv4();
 
     // grab data from the db.json & push the newitem into the database
-    let grocerydata = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let groceryData = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
-    grocerydata.push(newGroceryitem);
+    groceryData.push(newGroceryItem);
 
     // write to db.json
-    fs.writeFileSync('./db/db.json', JSON.stringify(grocerydata));
+    fs.writeFileSync('./db/db.json', JSON.stringify(groceryData));
     console.log('\nSuccesfully added the new groceryitem to db.json!');
 
-    res.json(grocerydata);
+    res.json(groceryData);
 });
 
 //Replace filename here 
-router.delete('/api/REplace filename/:id', (req,res) => {
+router.delete('/api/homepage/:id', (req,res) => {
     // access data note from client
     let id = req.params.id;
     
     console.log("this is gonna delete ur grocery item");
 
-    let grocerydata = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let groceryData = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
     // can use filter or for loop
-    for(let i=0; i<grocerydata.length;i++){
-        if(id == grocerydata[i].id){
-            grocerydata.splice(i,1);
+    for(let i=0; i<groceryData.length;i++){
+        if(id == groceryData[i].id){
+            groceryData.splice(i,1);
 
         // write to db.json
             fs.writeFileSync('./db/db.json', JSON.stringify(grocerydata));
             console.log('\nSuccesfully deleted the item!');
 
-            res.json(grocerydata);
+            res.json(groceryData);
             return;
         }
     }
