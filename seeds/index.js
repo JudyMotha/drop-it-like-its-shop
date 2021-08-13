b9ffda82f37960d31f4b5f1ac8e14733be550996
Ishya Casukhela <casukhelai@gmail.com>
2021-08-13 19:37:51 -0400

    changed the calls for the model

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,8 @@
-const sequelize = require('../config/connection');
-const Grocery = require('../models/Grocery');
-const groceryData = require('./grocery-seeds.json');
+// What is this file doing?
+// Fill in here: 
+const sequelize = require('../config/configuration');
+const { Grocery } = require('../models/');
+const item = require('./item.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
   await Grocery.bulkCreate(groceryData, {
