be03bc6c94bd031847860fcf48144a91c5bd571c
Alyson Orta <alyson.orta@gmail.com>
2021-08-14 11:33:54 -0400

    added code for registration

--- public/js/js/login.js
+++ public/js/js/login.js
@@ -14,20 +14,42 @@ const loginForm = async (event) => {
             headers: { 'Content-Type': 'application/json'},
         });
 
-        //if response is successful - go back to home page
+        //if response is successful - take user to homepage
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/homepage');
         } else {
             alert('Failed to log in');
         }
     }
 };
 
-
+//Create function to handle the register form created in login.handlebars
 const registerUserForm = async (event) => {
     event.preventDefault();
 
-    const name = 
-}
+    //Grab user input from registration fields
+    const name = document.querySelector('#name-register').value.trim();
+    const email = document.querySelector('#email-register').value.trim();
+    const password = document.querySelector('#password-register').value.trim();
 
-document.querySelector('.login-form').addEventListener('submit', loginForm);
+    //if values are in each field, post values to json data
+    if (name && email && password) {
+        const response = await fetch ('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        //if response is successful - take user to homepage
+        if (response.ok) {
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+
+//Add event listeners for submit buttons
+document.querySelector('.login-container').addEventListener('submit', loginForm);
+document.querySelector('.register-form').addEventListener('submit', registerUserForm);
