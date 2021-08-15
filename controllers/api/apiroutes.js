b4f7a7b27a7cc283d29ac20bf6d05e9e35b6e0ad
Alyson Orta <alyson.orta@gmail.com>
2021-08-15 13:35:33 -0400

    working on routes - code is not finished but pushing so teammates can review

--- controllers/api/apiroutes.js
+++ controllers/api/apiroutes.js
@@ -1,26 +1,55 @@
 //API routes// need to add this in package.json
 const router = require('express').Router();
-const fs = require('fs');
+// const fs = require('fs');
 const { GroceryItems } = require('../../models');
 
 
-// need to send the data to mysql
-router.post('/api/homepage', async (req,res) => {
-    //get  grocery item from end user  (client end)
+//CREATE a grocery item
+router.post('/', async (req, res) => {
     try {
-        const newGroceryItem = await GroceryItems.create({
-            list_id: req.body.list_id,
+        const groceryData = await GroceryItems.create({
             item: req.body.item,
-        })
-        res.json(newGroceryItem);
-    } catch(err){
-        console.log(err);
-        res.status(500).json(err);
+        });
+        res.status(200).json(groceryData)
+    } catch (err) {
+        res.status(400).json(err);
     }
+});
+//CREATE a grocery item
+// router.post('/', (req,res) => {
+//     GroceryItems.create(req.body)
+//     .then((newItem) => {
+//         res.json(newItem);
+//     })
+//     .catch((err) => {
+//         res.json(err);
+//     });
+// });
+
+//READ grocery items
+router.get('/', async (req, res) => {
+    GroceryItems.findAll().then((newItem) => {
+        res.json(newItem);
+    });
+});
+
+// // need to send the data to mysql
+// router.post('/api/homepage', async (req,res) => {
+//     //get  grocery item from end user  (client end)
+//     try {
+//         const newGroceryItem = await GroceryItems.create({
+//             list_id: req.body.list_id,
+//             item: req.body.item,
+//         })
+//         res.json(newGroceryItem);
+//     } catch(err){
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
    
 
     
-});
+// });
 
 // router.get('/api/groceryitems', async (req,res) => {
 //     try {
